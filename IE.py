01b0478ca8aa8245cea4e374ae4646db8067d63c
Javed Nahim Islam <jnahim@LOCAL.innovative-technology.co.uk>
2021-09-08 10:04:15 +0100

    Just looking at some minor changes to be made, leaving this as to be done

--- IE.py
+++ IE.py
@@ -6,10 +6,18 @@ import xlsxwriter
 from tkinter import*
 from tkinter import filedialog
 from tkinter import messagebox
+from ttkthemes import ThemedTk
 from tkinter import ttk
 
 
-
+def window(main):
+    main.title("Extraction Program")
+    main.update_idletasks()
+    width = main.winfo_width()
+    height = main.winfo_height()
+    x = (main.winfo_screenwidth()//2) - (width //2)
+    y = (main.winfo_screenheight()//2) - (height//2)
+    main.geometry("{}x{}+{}+{}".format(width,height,x,y))
 
 
 def extract_images_from_docx(path_to_file,images_folder,get_text=False):
@@ -19,7 +27,7 @@ def extract_images_from_docx(path_to_file,images_folder,get_text=False):
 
 def quit():
     root.destroy()
-                
+               
 def extract_images():
     beginButton["state"] = DISABLED
     docx = filedialog.askopenfilename(filetypes = [("Docx files","*.docx")])
@@ -52,11 +60,13 @@ def extract_images():
                             os.rename(filename,new_name)
                             count = count + 1
                             response.config(text = "Done!")
+                            response.after(2000,clear_label)
                         except FileExistsError as error:
-                            response.config(text = "Images already exists with that name")
                             if filename != new_name:
                                 os.remove(filename)
-        
+                            
+                                print("Identical Images which is: " + str(filename))
+
             except ValueError:
                 response.config(text = "Please enter an integer age value")
                 age_window.after(2000,clear_label)
@@ -67,20 +77,20 @@ def extract_images():
             decision = text_input
             data = extract_images_from_docx(path_to_file,images_folder,get_text =True)
             age_window = Toplevel()
-            age_window.geometry("400x400")
+            window(age_window)
             age_window.title("Age of Dataset")
-            age_label = Label(age_window, text = "Enter the age of the Dataset").grid(row = 2, column = 2)
-            age_prompt = Entry(age_window, width = 20)
+            age_label = ttk.Label(age_window, text = "Enter the age of the Dataset").grid(row = 2, column = 2)
+            age_prompt = ttk.Entry(age_window, width = 20)
             age_prompt.grid(row = 0, column = 50) 
-            age_button = Button(age_window, text = "Proceed",command = age_rename)
+            age_button = ttk.Button(age_window, text = "Proceed",command = age_rename)
             age_button.grid(row = 0, column = 40,pady = 5)
-            quit_button = Button(age_window, text = "Close", command = quit)
+            quit_button = ttk.Button(age_window, text = "Close", command = quit)
             quit_button.grid(row = 2,column = 40)
-            response = Label(age_window, text = "")
+            response = ttk.Label(age_window, text = "")
             response.grid(row = 1, column = 40)
 
             text_window = Toplevel()
-            text_window.geometry("400x400")
+            window(age_window)
             text_window.title("Extracted Text")
 
             # Create A Main Frame
@@ -112,6 +122,7 @@ def extract_images():
 
             data_output = Label(second_frame, text = data)
             data_output.grid(row = 0, column = 0)
+
             
 
                                                            
@@ -119,26 +130,29 @@ def extract_images():
             decision = text_input
             data = extract_images_from_docx(path_to_file,images_folder,get_text =False)
             age_window = Toplevel()
-            age_window.geometry("400x400")
+            window(age_window)
             age_window.title("Age of Dataset")
             #age_label = Label(age_window, text = "Enter the age of the Dataset").grid(row = 2, column = 2)
-            age_prompt = Entry(age_window, width = 20)
+            age_prompt = ttk.Entry(age_window, width = 20)
             age_prompt.grid(row = 0, column = 50) 
-            age_button = Button(age_window, text = "Proceed",command = age_rename)
+            age_button = ttk.Button(age_window, text = "Proceed",command = age_rename)
             age_button.grid(row = 0, column = 40,pady = 5)
-            response = Label(age_window, text = "")
+            response = ttk.Label(age_window, text = "")
             response.grid(row = 1, column = 40)
-            quit_button = Button(age_window, text = "Quit", command = quit)
+            quit_button = ttk.Button(age_window, text = "Quit", command = quit)
             quit_button.grid(row = 1, column = 40)
-            
-            
-root = Tk()
-root.wm_title("Image Extractor!")
-beginButton = Button(root,text = "Begin!", width = 6, command = extract_images)
-beginButton.grid(row = 0,column = 1)
-quit_button = Button(root, text = "Quit", command = quit)
-quit_button.grid(row = 0,column = 2)
 
+
+
+root = ThemedTk(theme = "black")
+window(root)            
+
+root.wm_title("Image Extractor!")
+beginButton = ttk.Button(root,text = "Begin!", width = 6, command = extract_images)
+beginButton.grid(row = 0,column = 1)
+
+quit_button = ttk.Button(root, text = "Quit", command = quit)
+quit_button.grid(row = 0,column = 2)            
 root.mainloop()
 
 
