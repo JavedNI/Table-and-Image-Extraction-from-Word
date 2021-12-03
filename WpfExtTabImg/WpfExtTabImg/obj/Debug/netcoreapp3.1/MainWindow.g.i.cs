3dbcad03ade82f7bb591ddd52531a312071c3115
Euan-Benson <euanbson@gmail.com>
2021-12-03 13:58:51 +0000

    fixes

--- WpfExtTabImg/WpfExtTabImg/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ WpfExtTabImg/WpfExtTabImg/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -68,7 +68,7 @@ namespace WpfExtTabImg {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfExtTabImg;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfExtTabImg;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
